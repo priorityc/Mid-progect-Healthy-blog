3d1ef9f5d23b211d877668e53900765de56060c6
priorityc <petya83dimitrova@gmail.com>
2022-03-08 14:59:27 +0000

    checkout integration finished

--- project.js
+++ project.js
@@ -1,66 +1,49 @@
 
 //geting the data
-//inputPerson=document.querySelector('button')
-let personName;
-let personEmail;
 
-func1 {
-  if (personName.length==0) {
-    alert('Please fill your name');
-  }else{
-    checkUserinputs();
+function validateForm() {
+   personName=document.forms['myform']['fname'].value;
+ if (personName == ('')){
+    alert("Name must be filled out");
   }
+   personEmail = document.forms['myform']['e-mail'].value;
+   if (personEmail.indexOf('@') == -1){
+    alert("Email must be filled out");
   }
-document.querySelector('#subscribe').addEventListener('click',func1)
-checkUserinputs = () => {
-  let personName = document.getElementById('input1').value;
-	let personEmail = document.getElementById('input2').value;
-	if (personEmail.length == 0) {
-		throw new Error('please provide an email address');
-	} else if (userEntry.indexOf('@') === -1) {
-		throw new Error('please provide a valid email address');
-	}
-	
-};
-document.querySelector('#button').innerHTML = `<h3 style="color:green"> Your email is: </h3>` + personEmail;
-validateEntry = () => {
-	
-  try {
-		checkUserinputs();
-	} catch (error) {
-		document.getElementById('console').innerHTML = `<h3 class="error"> Warning: </h3>` + error.message;
-	}
-};
+}
 
-document.getElementById('subscribe').addEventListener('click', validateEntry);
+//if payid func
+/*paypal.Buttons({
+  onCancel: function (data) {
+    // Show a cancel page, or return to cart
+  }
+}).render('#paypal-button-container');
+
+let element=document.querySelector('hiden');*/
 
 
-
-
-/*//turn data Jason into object
-fetch('./posts.json')
-  .then((res) => res.json()) 
+//stake the data
+fetch(`./posts.json`)
+  .then((res) => res.json())
   .then((data) => {
-   //console.log(data);
-  });
+     console.log(data);
+
+   if (!localStorage.getItem('image1'));
+   funcLocalData(data.items);
+    }
+    //funcCreateCards();
+  );
+
+   //console.log(image1);
 
 
-//store that data into local storige
- 
-funcCreatepost = () => {
-  let tempPostData = JSON.parse(localStorage.getItem('data'))
-  let tempPost1Data = '';
-  tempPostData.map((nMD) => {
-    tempPost1Data += `<div class="PostCard">
-    <img
-      src="${nMD.image1}"
-    />
-    <div class="postCardDetails">
-      <h2>${nMD.image1}</h2>
-      <p>${nMD.post}</p>
-      </div>`;
-  });
-  document.querySelector('body').innerHTML = tempPostData;
-};
-
-funcDisplaycard*/
+  funcLocalData = (posts) => {
+    let postData = [];
+  
+  posts.map((p) => {
+      postData.push({
+        image1: p.image1,
+        post: p.post,
+      });
+    }
+  )};
